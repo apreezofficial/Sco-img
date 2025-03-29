c8870c2b9abcb187a7120a14ffc2c4002c0c986d
Precious Adedokun <apreezofficial@gmail.com>
2025-03-29 13:52:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 //This file is restrited no one is allowed to copy anything here, message the author directly for more info
-const API_KEY = "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";
+//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
+const API_KEY = "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";
